e7c748fab4c535a926e4b7db332ce5c2b88b8960
IgorDv5 <igoraluno2005@gmail.com>
2026-05-20 21:49:54 -0300

    chore: Projeto Iniciado

---
+++ src/main/java/com/igor/walletManager/MyWalletManagerApplication.java
@@ -0,0 +1,13 @@
+package com.igor.walletManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWalletManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/walletManager/MyWalletManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.walletManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
