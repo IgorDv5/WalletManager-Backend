3acc9b7faafd981bf0a1321a9632e66afda28126
IgorDv5 <igoraluno2005@gmail.com>
2026-05-21 11:54:51 -0300

    fix : Correção de interface UserMapper

--- src/main/java/com/igor/walletManager/mappers/UserMapper.java
+++ src/main/java/com/igor/walletManager/mappers/UserMapper.java
@@ -4,15 +4,13 @@ import org.mapstruct.Mapper;
 
 import com.igor.walletManager.dtos.users.UserCreateDTO;
 import com.igor.walletManager.dtos.users.UserResponseDTO;
-import com.igor.walletManager.dtos.users.UserUpdateDTO;
 import com.igor.walletManager.entity.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 	
-	UserResponseDTO toDTO(User user);
-	
 	User toEntity(UserCreateDTO dto);
+	
+	UserResponseDTO toDTO(User user);
 
-	User toEntity(UserUpdateDTO dto);
 }
